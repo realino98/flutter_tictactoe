c75b99d83f6ec6d75e165f2dbeea923c84333374
Bernard <bernard.realino98@gmail.com>
2023-08-03 10:00:28 +0700

    BR : Basic Game Function OK

--- lib/home.dart
+++ lib/home.dart
@@ -12,74 +12,74 @@ class _TicTacToeState extends State<TicTacToe> {
   List<String> board = ["", "", "", "", "", "", "", "", ""];
   bool isPlayerOne = true;
   bool isPlaying = true;
-  String curCursor = "X";
+  String curCursor = "O";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("TicTacToe Game"),
       ),
-      body: Row(
+      body: Column(
         children: [
-          Column(
-            children: [
-              Row(
-                children: [
-                  BoardButton(0, board[0]),
-                  BoardButton(1, board[1]),
-                  BoardButton(2, board[2]),
-                ],
+          Expanded(
+            flex: 2,
+            child: GridView.builder(
+              itemCount: board.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisSpacing: 2,
+                mainAxisSpacing: 2,
+                crossAxisCount: 3,
               ),
-              Row(
-                children: [
-                  BoardButton(3, board[3]),
-                  BoardButton(4, board[4]),
-                  BoardButton(5, board[5]),
-                ],
-              ),
-              Row(
-                children: [
-                  BoardButton(6, board[6]),
-                  BoardButton(7, board[7]),
-                  BoardButton(8, board[8]),
-                ],
-              ),
-            ],
+              itemBuilder: (context, index) {
+                return boardButton(index);
+              },
+            ),
           ),
-          Text(isPlaying ? "Game is Still Playing" : "Game Stopped"),
-          Text(isPlaying ? "" : "Congratulation! Player ${curCursor} Wins!"),
+          Expanded(
+            flex: 1,
+            child: Text(
+              !isPlaying
+                  ? "Congratulation, player ${curCursor} wins!"
+                  // Inverting Player who is currently playing
+                  : isPlayerOne
+                      ? "Player O turn"
+                      : "Player X turn",
+              style: TextStyle(
+                fontSize: 35,
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 
-  Card BoardButton(int id, String val) {
-    return Card(
-      elevation: 1,
-      child: FloatingActionButton(
-        onPressed: () {
-          print("Board ${id} X");
-          setState(
-            () {
-              if (board[id] == "" && isPlaying) {
-                if (isPlayerOne) {
-                  curCursor = "O";
-                } else {
-                  curCursor = "X";
-                }
-                isPlayerOne = !isPlayerOne;
-                board[id] = curCursor;
-                isPlaying = gameCheck(board, curCursor, isPlaying);
-              }
-            },
-          );
-        },
-        child: Container(
-          child: Center(
-            child: Text(board[id]),
+  boardButton(int id) {
+    return GestureDetector(
+      onTap: () {
+        setState(
+          () {
+            if (board[id] == "" && isPlaying) {
+              isPlayerOne = !isPlayerOne;
+              curCursor = isPlayerOne ? "X" : "O";
+              // if (isPlayerOne) {
+              //   curCursor = "O";
+              // } else {
+              //   curCursor = "X";
+              // }
+              board[id] = curCursor;
+              isPlaying = gameCheck(board, curCursor, isPlaying);
+            }
+          },
+        );
+      },
+      child: Container(
+        color: isPlaying ? Colors.black26 : Colors.green[300],
+        child: Center(
+          child: Text(
+            board[id],
+            style: TextStyle(fontSize: 150),
           ),
-          height: 200,
-          width: 200,
         ),
       ),
     );
